db85ad7a740c2e549b10d2d63e3b8331841f06a6
HenryFelic07 <hpfelicita@gmail.com>
2022-03-12 10:44:36 -0500

    segundo comit

--- app/src/main/java/com/example/fullcolordesign/MainActivity.kt
+++ app/src/main/java/com/example/fullcolordesign/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.fullcolordesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var mainBotonIngresar= findViewById<Button>(R.id.mainButtonIngresar)
+        var mainEditTxtEmail= findViewById<EditText>(R.id.mainEditTextEmail)
+        var mainEditTxtPassword= findViewById<EditText>(R.id.mainEditTextPassword)
+        var mainTextFaildEmail = findViewById<TextInputLayout>(R.id.mainTextFieldEmail)
+
+        mainBotonIngresar.setOnClickListener(){
+            if (mainEditTxtEmail.text.toString().trim() == "")
+            {
+                mainTextFaildEmail.error= getString(R.string.error)
+            } else{ mainTextFaildEmail.error= null }
+        }
+
+
     }
 }
